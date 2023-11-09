9dcb1537c592c5882ab60a2f83e4862016dcfbe1
Matthew Campbell <hyper@hyperworks.nu>
2023-11-09 23:28:24 +0700

    add more common libs for llama2.cpp

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 5;
+char const *LLAMA_COMMIT = "a85909e";
+char const *LLAMA_COMPILER = "Apple clang version 15.0.0 (clang-1500.0.40.1)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin23.0.0";
